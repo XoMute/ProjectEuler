363778fa77babbfac7b6f0b5d7ba9483034d82ae
Dmytro Khomutnyk <dhomutnik@gmail.com>
2019-01-11 20:16:27 +0200

    Done 11

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import com.xomute.euler010.Euler010;
+import com.xomute.euler011.Euler011;
 
 public class Main {
 
   public static void main(String[] args) {
     long time = System.nanoTime();
-    System.out.println(Euler010.sum(2000000));
+    System.out.println(Euler011.greatestProduct());
     System.out.println("Time in ms: " + ((System.nanoTime() - time) / 1000000f));
   }
 }
